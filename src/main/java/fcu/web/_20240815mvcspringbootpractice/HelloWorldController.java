a06005d80220e6c786830aa2bc360cf368a8486c
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-15 23:32:33 +0800

    v1.21 新增了一個 greeting2 方法。       新方法使用不同的 URL 路徑 "/greeting2"。       新方法提供了一個不同的默認問候語 "form MVC_Java_World"。       兩個方法都使用相同的 "greeting" 視圖來渲染結果 cnp

--- src/main/java/fcu/web/_20240815mvcspringbootpractice/HelloWorldController.java
+++ src/main/java/fcu/web/_20240815mvcspringbootpractice/HelloWorldController.java
@@ -3,13 +3,20 @@ package fcu.web._20240815mvcspringbootpractice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
-
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+
+    }
+
+    @GetMapping("/greeting2")
+    public String greeting2(@RequestParam(name = "name", required = false, defaultValue = "form MVC_Java_World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
